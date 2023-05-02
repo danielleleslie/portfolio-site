229581443e7b726a30fc451d9b06c83a881a7f92
Danielle Leslie <danielle.does.code@gmail.com>
2023-05-02 15:39:08 -0500

    about me card

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,23 @@
 import Footer from './Footer';
+import logo from "./logo.png";
+
 
 export default function AboutMe() {
     return (
-        <div>
-            <h1>About Me</h1>
+        <div className='w-full py-[10rem] px-4'>
+            <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
+                <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 border-t-2 border-gray-100 bg-white object-center'>
+                    <img className='w-20 mx-auto mt-[1rem] bg-transparent' src={logo} alt="/"></img>
+                    <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
+                    <p className='text-center text-4xl font-bold'>$149</p>
+                    <div className='text-center font-medium'>
+                        <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
+                        <p className='py-2 border-b mx-8'>1 Granted User</p>
+                        <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
+                    </div>
+                    <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Start Trial</button>
+                </div>
+            </div>
         </div>
     );
-}
+};
